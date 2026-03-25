acf52cd8ae1bdfaed6d0871bca0f8890dbe4f495
kadriyeatuncel <kadriyeatuncel@gmail.com>
2026-03-25 21:58:15 +0100

    les5_he added

---
+++ les4.py
@@ -0,0 +1,51 @@
+"""name = "Kadriye"
+name_long = len(name)
+
+print(name)
+
+print (f"The name '{name}' is {name_long}" long.)
+# ---------------------------------------------------------------------------------
+name= "Aziz Talbi"
+len_name =len {len_name}
+fourth_letter =name [3]
+print(f"the fourth letter of the name is {fourth_letter}")
+
+print ({name[-5]})"""
+# ------------------------------------------------------------------------------------------------------------------------------------
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"  
+# Kadriye Tuncel 'bosluk istiyorsa +"" yap
+"""my_name = alphabet[10] + alphabet[0] + alphabet[3] + alphabet[17] + alphabet[8] + alphabet[24] + alphabet[4]
+print(my_name)
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------
+name = "Aziz Talibi"
+print (name.count{"az"})
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+
+str1 = 'abc'
+str2 = 'xyz'
+
+new_str1 = str2[:2] + str1[2:]
+new_str2 = str1[:2] + str2[2:]
+result = new_str1 + " " + new_str2
+
+print(result)
+# -----------------------------------------------------------------------------------------------------------------------
+names = ["Desi", "Martha", "Joan"] 
+print(names)
+# --------------------------------------------------------------
+names.append("Sylvia"))
+print("Appended list:, names")
+names.insert(2,"Kadriye")
+print(print("Appended list:, names"))"""
+# -------------------------------------------------------------------------------------------------------------------
+names = ["Suchin", "Martha", "Joan", "Kadriye"]
+names.append("Sylvia")
+names [2] = "Vesna" 
+names.pop(0)
+print("Number of students:", len(names)) 
+names.sort()
+print("Sorted names list:", names) 
+# --------------------------------------------------------------------------
